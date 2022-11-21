c6fa747d20a4c78d8c514f8ce1158086210b88a0
Thomas Saldana <saldana.tom@gmail.com>
2022-11-21 00:37:42 -0800

    Added picture

--- client/src/components/MovieList.jsx
+++ client/src/components/MovieList.jsx
@@ -3,7 +3,10 @@ import MovieEntry from './MovieEntry.jsx';
 
 const MovieList = ({movies, toggle}) => {
   return (
-    <div className='movieList'>{movies.length ? movies.map((movie, index) => <MovieEntry movie={movie} key={index} toggle={toggle}/>) : <img src="client/dist/ZC101cU.jpg"></img>}</div>
+    <div className='movieList'>{movies.length
+      ? movies.map((movie, index) => <MovieEntry movie={movie} key={index} toggle={toggle}/>)
+      : <img src="https://i.imgur.com/2UGmMSw.png"></img>}
+    </div>
   );
 };
 
